8ddfbba4ee3ece32d5c12246594af5953f4b011f
Micaeli82 <micaeliolieveira@gmail.com>
2024-11-18 20:17:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Como eram feito as roupas?',
+    'as roupas eram feitas do couro dos animais. A partir da invenção do tear aparelho que permite fazer tecido'
+)
+
+criaCartao(
+    'Geral',
+    'Como foi criado o halloween?',
+    'O Halloween é originário de uma festa dos celtas chamada Samhain'
+)
+
+criaCartao(
+    'Geral',
+    'Do que é feito o vidro??',
+    'A fórmula conhecida do vidro contém areia de sílica sódio e cálcio'
+)
+
+criaCartao(
+    'Geral',
+    'Quem inventou o macarrão?',
+    'historiadores apontam que a massa é uma invenção chinesa'
+)
